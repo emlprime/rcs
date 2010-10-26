d74bf7f0219d1c3fbbf3e314d0bd99d76bed416c
Peter Stradinger <peter@rightscale.com>
2010-10-26 00:27:07 -0700

    got a good loop for the calendar

--- event/models.py
+++ event/models.py
@@ -31,3 +31,23 @@ class Event(models.Model):
 
         return range(1, end + 1)
 
+    @classmethod
+    def get_weeks_for_month(cls, date_str=None):
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        weeks = [[]]
+        # pad the front of the calendar
+        [weeks[-1].append(None) for i in range(date.weekday()-1)]
+
+        for day in cls.get_days_of_month(date_str):
+            date = datetime(date.year, date.month, day)
+            if date.weekday() == 0 and len(weeks) > 0:
+                weeks.append([])
+            events = []
+            weeks[-1].append((day, events))
+            
+        # pad the end of the calendar
+        [weeks[-1].append(None) for i in range(6-date.weekday())]
+        
+        return weeks
+
+
