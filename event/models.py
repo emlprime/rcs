8b656e8afa3889c8493336cf97f36039f3741b72
Peter Stradinger <peter@rightscale.com>
2010-10-26 00:02:40 -0700

    accounting for months

--- event/models.py
+++ event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Event(models.Model):
     """ Event class for a calendar
@@ -17,4 +18,16 @@ class Event(models.Model):
         events = cls.objects.filter(date_start=date)
         return events
 
+    @classmethod
+    def get_days_of_month(cls, date_str=None):
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        
+        # 30 days hath september, april, june and november
+        # all the rest have 31
+        end = 30 if date.month in [4,6,9,11] else 31
+        # except for little February
+        if date.month == 2:
+            end = 28
+
+        return range(1, end + 1)
 
